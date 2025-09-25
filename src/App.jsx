efd3d3241a8cf464dd40337f4f9a8cd2f9cbe303
David <jejesus484@email.com>
2025-09-25 15:44:12 -0500

    intento de correccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,23 @@
-import Header from "./componentes/Header";
-import Footer from "./componentes/Footer";
-import Main from "./componentes/Main";
+import Header from "./componentes/Header.jsx";
+import Footer from "./componentes/Footer.jsx";
+import Main from "./componentes/Main.jsx";
 
 function App() {
   return (
-    <div class="flex flex-col p-4 h-full w-full">
+    <div className="flex flex-col p-4 h-full w-full">
       <Header />
       <div className="grid grid-cols-2 md:grid-cols-3 px-2 py-5 gap-2 justify-center items-center w-full">
         <Main
           name="Bill Mahoney"
           job="PRODUCT OWNER"
-          src="../public/images/photo1.png"
+          src="/images/photo1.png"
         />
-        <Main
-          name="Saba Cabrera"
-          job="ART DIRECTOR"
-          src="../public/images/photo2.png"
-        />
-        <Main
-          name="Shae Le"
-          job="TEACH LEAD"
-          src="../public/images/photo3.png"
-        />
-        <Main
-          name="Skylah Lu"
-          job="UX DESIGNER"
-          src="../public/images/photo4.png"
-        />
-        <Main
-          name="Griff Richards"
-          job="DEVERLOPER"
-          src="../public/images/photo5.png"
-        />
-        <Main
-          name="Stan John"
-          job="DEVERLOPER"
-          src="../public/images/photo6.png"
-        />
+
+        <Main name="Saba Cabrera" job="ART DIRECTOR" src="/images/photo2.png" />
+        <Main name="Shae Le" job="TEACH LEAD" src="/images/photo3.png" />
+        <Main name="Skylah Lu" job="UX DESIGNER" src="/images/photo4.png" />
+        <Main name="Griff Richards" job="DEVERLOPER" src="/images/photo5.png" />
+        <Main name="Stan John" job="DEVERLOPER" src="/images/photo6.png" />
       </div>
 
       <Footer />
--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,13 +1,19 @@
-
-
-export default function Main(props) {
+export default function Main({ name, src, job, alt }) {
   return (
     <div className="flex sm:even:pt-20 md:even:pt-0 justify-center">
-        <div className="flex flex-col  h-full">
-            <img className="h-[220px]  sm:h-[270px] w-[200px]" src={props.src} alt={props.alt} />
-            <h3><strong>{props.name}</strong></h3>
-        </div>
-        <h3 className="font-serif" style={{writingMode: "vertical-rl"}}>{props.job}</h3>
+      <div className="flex flex-col  h-full">
+        <img
+          className="h-[220px]  sm:h-[270px] w-[200px]"
+          src={src}
+          alt={alt}
+        />
+        <h3>
+          <strong>{name}</strong>
+        </h3>
+      </div>
+      <h3 className="font-serif" style={{ writingMode: "vertical-rl" }}>
+        {job}
+      </h3>
     </div>
-  )
+  );
 }
